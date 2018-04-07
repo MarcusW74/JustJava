f4957652d534102528b796ffc2fb74f7cafb7724
MarcusW74 <35531032+MarcusW74@users.noreply.github.com>
2018-04-07 08:18:34 -0400

    Updated app tp add checkbox

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,8 +1,19 @@
 package com.example.android.justjava;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import static android.util.Log.*;
+
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +21,80 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    int quantity = 0;
+    int price;
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        CheckBox whippedCreamCheckbox = (CheckBox)findViewById(R.id.whipped_cream);
+        boolean hasWhippedCream = whippedCreamCheckbox.isChecked();
+        int price = calculatePrice();
+        displayMessage(createOrderSummary(price, hasWhippedCream));
+
+    }
+
+    /**
+     * Create summary of the order
+     *
+     * @param price of the order
+     * @param addWhippedCream is whether or not the user wants whipped cream
+     * @return text summary
+     */
+
+    private String createOrderSummary(int price, boolean addWhippedCream) {
+        String priceMessage = "Name: Buon Caffe";
+        priceMessage += "\nAdd whipped cream? " +addWhippedCream;
+        priceMessage += "\nQuantity: " + quantity;
+        priceMessage += "\nTotal: $" + price;
+        priceMessage += "\nThank you!";
+
+        return priceMessage;
+    }
+    /**
+     * Calculates the price of the order
+     *
+     * @return total price
+     */
+
+    private int calculatePrice() {
+        return quantity * 5;
+    }
+
+    /**
+     * This method is called when the plus button is clicked.
+     */
+    public void increment(View view) {
+        quantity = quantity + 1;
+        displayQuantity(quantity);
+    }
+
+    /**
+     * This method is called when the minus button is clicked.
+     */
+    public void decrement(View view) {
+        quantity = quantity - 1;
+        if (quantity <= 0){
+            quantity = 0;
+        }
+        displayQuantity(quantity);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void displayQuantity(int numberOfCoffees) {
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + numberOfCoffees);
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        Button orderSummaryTextView = findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
+    }
+
+
 }
